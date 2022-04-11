1ec7fe012e65534da9e20debff0fc2918682f145
StylesP23 <riley.pleas@gmail.com>
2022-04-10 20:47:48 -0700

    installed mongoose

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://ddthies:Gourmacracy123@netcentriccluster.26wkw.mongodb.net/Gourmacracy?retryWrites=true&w=majority';
 
+
 mongoose
     .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -10,6 +11,8 @@ mongoose
     .catch(error => {
         console.log('Connection Error: ${err.message}');
     });
+
+
 
 const db = mongoose.connection;
 
